dd70e02f57f3a0a09d95ad211a1e942479c7a4de
coder-avril <avril_for_me@yahoo.co.jp>
2022-03-26 12:40:20 +0900

    first commit

---
+++ LearnVuejs03/02-tabbar/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <tab-bar>
+      <tab-bar-item link="/home">
+        <img src="~assets/tabbar/home.svg" slot="icon" alt="">
+        <img src="~assets/tabbar/home_active.svg" slot="active-icon">
+        <div slot="text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item link="/category">
+        <img src="~assets/tabbar/category.svg" slot="icon" alt="">
+        <img src="~assets/tabbar/category_active.svg" slot="active-icon">
+        <div slot="text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item link="/shopcart">
+        <img src="~assets/tabbar/shopcart.svg" slot="icon" alt="">
+        <img src="~assets/tabbar/shopcart_active.svg" slot="active-icon">
+        <div slot="text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item link="profile">
+        <img src="~assets/tabbar/profile.svg" slot="icon" alt="">
+        <img src="~assets/tabbar/profile_active.svg" slot="active-icon">
+        <div slot="text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+  import TabBar from 'components/tabbar/TabBar'
+  import TabBarItem from 'components/tabbar/TabBarItem'
+
+  export default {
+    name: 'app',
+    components: {
+    	TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style>
+  @import "assets/style.css";
+</style>
---
+++ LearnVuejs03/02-tabbar/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	configureWebpack: {
+		// resolve通常用来配置一些路径解析
+		resolve: {
+			alias: {
+				'components': '@/components',
+				'assets': '@/assets',
+				'views': '@/views'
+			}
+		}
+	}
+}
